2610d1617a55ff9b61ec469245d0102435bdec9a
Xavier Hanin <xavier.hanin@4sh.fr>
2022-01-10 00:06:20 +0100

    setup k3d cluster to expose ingress, now that it is bound to envoy

--- scripts/create-k3d-cluster.sh
+++ scripts/create-k3d-cluster.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "create cluster"
-k3d cluster create grpc-example-cluster -p "8098:30080@agent:0" --agents 2
+k3d cluster create grpc-example-cluster -p "8098:80@loadbalancer" -p "8099:30080@agent:0" --agents 2
 
