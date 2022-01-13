81c6b2f00d1e03a416f14e9cc4bd9ead33af332b
Xavier Hanin <xavier.hanin@4sh.fr>
2022-01-13 17:56:31 +0100

    use auth0 to get access token to use for grpc-web call
    no verification / use on server side yet

--- modules/web-bo/app/src/stock/Stock.js
+++ modules/web-bo/app/src/stock/Stock.js
@@ -1,48 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useAuth0 } from "@auth0/auth0-react";
 
 import {LoadWinecellarStockRequest} from "../proto/example/winecellar/winecellar_pb"
 import {WinecellarServiceClient} from "../proto/example/winecellar/winecellar_grpc_web_pb"
 
 import StockItem from "./stockitem/StockItem";
 
-class Stock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            items: []
-        };
-    }
+function Stock() {
+    const [items, setItems] = useState([]);
 
-    componentDidMount() {
+    const {getAccessTokenSilently} = useAuth0();
+
+    const loadStock = async () => {
         const enableDevTools = window.__GRPCWEB_DEVTOOLS__ || (() => {});
         var winecellarService = new WinecellarServiceClient("http://sample-grpcweb.k3d.localhost:8098");
         enableDevTools([winecellarService]);
 
-        winecellarService.loadStock(new LoadWinecellarStockRequest().setWinecellarid("111"), {}, (err, response) => {
+        const token = await getAccessTokenSilently();
+        console.log(token)
+
+        winecellarService.loadStock(new LoadWinecellarStockRequest().setWinecellarid("111"), {"Authorization": "Bearer " + token}, (err, response) => {
             if (err) {
                 console.log("error", err);
                 return;
             }
-            this.setState({items: response.getItemsList().map( (item) =>
-                    <StockItem
-                        key={item.getWineryref().getName()}
-                        wineryName={item.getWineryref().getName()}
-                        quantity={item.getQuantity()} />
-                )});
+            setItems(response.getItemsList().map( (item) =>
+                <StockItem
+                    key={item.getWineryref().getName()}
+                    wineryName={item.getWineryref().getName()}
+                    quantity={item.getQuantity()} />
+            ));
         });
-    }
+    };
 
-    render() {
-        return (
-            <div className="content">
-                <p className="title">Cellar Stock</p>
-                <p className="subtitle">My lovely bottles</p>
-                <div className="content">
-                    {this.state.items}
+    return (
+        <div className="content">
+            <div className="title level">
+                <div className="level-left">
+                    <div className="level-item">
+                        Cellar Stock
+                    </div>
+                </div>
+                <div className="level-right">
+                    <div className="level-item">
+                        <button className="button is-link" onClick={loadStock}>
+                            Load stock
+                        </button>
+                    </div>
                 </div>
             </div>
-        );
-    }
+            <p className="subtitle">My lovely bottles</p>
+            <div className="content">
+                {items}
+            </div>
+        </div>
+    );
 }
 
 export default Stock;
