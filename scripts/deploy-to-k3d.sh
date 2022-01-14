05965c0431e58f48ea64bc8e81d01d37a6f1414d
Xavier Hanin <xavier.hanin@4sh.fr>
2022-01-14 09:14:41 +0100

    prepare add linkerd
    commented out just to be able to add it easily in a demo

--- scripts/deploy-to-k3d.sh
+++ scripts/deploy-to-k3d.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 echo "building k8s descriptors and applying to k3d cluster"
-kustomize build k8s/environments/k3d/ | kubectl apply -n grpc-demo -f -
+kustomize build k8s/environments/k3d/ | kubectl apply -f -
+#kustomize build k8s/environments/k3d/ | linkerd inject - | kubectl apply -f -
 
 echo "done - check state with kubectl get pods"
 kubectl get pods -n grpc-demo
