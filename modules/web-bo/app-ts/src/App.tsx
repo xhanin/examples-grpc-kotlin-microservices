55cdfd510ef9b5ac92ada487086ddd3a723f2a97
Xavier Hanin <xavier.hanin@4sh.fr>
2022-01-14 21:50:17 +0100

    web-ts: minor app ui improvement

--- modules/web-bo/app-ts/src/App.tsx
+++ modules/web-bo/app-ts/src/App.tsx
@@ -80,10 +80,10 @@ function App() {
               </div>
               <div className="tile is-parent">
                 <article className="tile is-child notification is-info">
-                  <p className="title">Middle tile</p>
-                  <p className="subtitle">With an image</p>
+                  <p className="title">Image of the week</p>
+                  <p className="subtitle">Nappa Valley wine cellar</p>
                   <figure className="image is-4by3">
-                    <img src="https://bulma.io/images/placeholders/640x480.png" />
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/f/fc/NorCal2018_-020_Sterling_Vineyards_-_Wine_Barrels_-_Napa_Valley_-S0240208.jpg" />
                   </figure>
                 </article>
               </div>
